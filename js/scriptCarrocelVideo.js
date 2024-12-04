72ead46c903dbf3d116491d0a1e980a75f55072d
JoaoViniciusG <joaovigu07@gmail.com>
2024-12-03 23:27:30 -0400

    Alteração de temas

--- js/scriptCarrocelVideo.js
+++ js/scriptCarrocelVideo.js
@@ -70,13 +70,5 @@ window.addEventListener("scroll", function () {
     let ancoraEscolhida = this.document.querySelector('#ancora-escolhida')
     let conteudo = document.querySelector('.div-logo-header')
     header.classList.toggle('rolagem', window.scrollY > 0)
-    if (window.innerWidth > 765) {
-        if (window.scrollY > 0) {
-            ancoraEscolhida.style.color = 'black'
-        } else{
-            ancoraEscolhida.style.color = 'rgb(255, 255, 255)'
-        }
-    }
-
     conteudo.classList.toggle('rolagem', window.scrollY > 100)
 })
